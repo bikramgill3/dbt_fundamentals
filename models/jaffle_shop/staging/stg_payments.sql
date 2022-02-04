89c2988b819d5c8963377a4c8dad34f1ef7774ba
b.gill_23@hotmail.com <b.gill_23@hotmail.com>
2022-02-04 23:23:35 +0000

    add stg_payments and fct_orders

---
+++ models/jaffle_shop/staging/stg_payments.sql
@@ -0,0 +1,14 @@
+with payments as (
+    
+    select 
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+	    `status` as payment_status,
+        amount,
+        created
+
+    from dbt-tutorial.stripe.payment
+)
+
+select * from payments
